cc9102541ae56b8271985feee7911c802bdfaec7
Jean Julien <me@jeanjulienhessouh.com>
2023-10-15 23:31:45 +0100

    created some instance variable to the class

--- tests/pages/get-started.ts
+++ tests/pages/get-started.ts
@@ -2,4 +2,8 @@ import { type Locator, type Page } from '@playwright/test';
 
 export class GetStartedPage {
     // ...
+    readonly page: Page;
+    readonly boardTitle: Locator;
+    readonly enterListTitle: Locator;
+    readonly boardLists: Locator
 }
